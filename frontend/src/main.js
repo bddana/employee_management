1ebf5068280e90a7beac87ef6cbb9d32eee59378
Chonghe Ping <pingchonghe@gmail.com>
2020-10-25 23:43:38 -0500

    second submit:Improve the Employee and Boat modules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router.js'
 import store from './store/store.js';
+import { Button, Table, Form, TableColumn, Popover, Tooltip } from "element-ui";
+Vue.use(Button);
+Vue.use(Table);
+Vue.use(Form);
+Vue.use(TableColumn);
+Vue.use(Popover);
+Vue.use(Tooltip);
 
 Vue.config.productionTip = false
 
