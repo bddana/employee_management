c4b83214e9d3ddfe24cd307e2f7f9d0f57f82c26
chonghe <pingchonghe@gmail.com>
2020-11-08 18:44:25 -0600

    Add function

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router.js'
 import store from './store/store.js';
-import { Button, Table, Form, TableColumn, Popover, Tooltip } from "element-ui";
-Vue.use(Button);
-Vue.use(Table);
-Vue.use(Form);
-Vue.use(TableColumn);
-Vue.use(Popover);
-Vue.use(Tooltip);
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// Vue.use(Button);
+// Vue.use(Table);
+// Vue.use(Form);
+// Vue.use(TableColumn);
+// Vue.use(Popover);
+// Vue.use(Tooltip);
+// Vue.use(Container);
+// Vue.use(Dialog);
 
 Vue.config.productionTip = false
 
--- frontend/src/views/Employee.vue
+++ frontend/src/views/Employee.vue
@@ -1,13 +1,53 @@
 <template>
-
-  <el-table
+  <container>
+  <el-header>
+    <el-row>
+      <el-button type="primary" @click="dialogVisible = true" round>New</el-button>
+      <span style="font-size:30px;color:blue">{{this.tableData.length}} Employees</span>
+      <el-dialog title="Add Employee" :visible.sync="dialogVisible" :append-to-body="true" width="30%">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="ID">
+            <el-input v-model="form.id"></el-input>
+          </el-form-item>
+          <el-form-item label="First Name">
+            <el-input v-model="form.firstName"></el-input>
+          </el-form-item>
+          <el-form-item label="Last Name">
+            <el-input v-model="form.lastName"></el-input>
+          </el-form-item>
+          <el-form-item label="TEL">
+            <el-input v-model="form.tel"></el-input>
+          </el-form-item>
+          
+          <el-form-item label="Date">
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
+          
+          <el-form-item label="Address">
+            <el-input type="textarea" v-model="form.address"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Conform</el-button>
+            <!-- <el-button @click="onCancel">Cancel</el-button> -->
+          </el-form-item>
+        </el-form>
+    </el-dialog>
+    </el-row>
+  </el-header>
+  <el-main>
+    <el-table
     :data="tableData"
     border
-    style="width: 100%"
+    style="width: 80%"
     :row-class-name="tableRowClassName">
     <el-table-column
-      :render-header="renderHeader"
-      fixed
+
       prop="id"
       label="ID"
       width="180">
@@ -41,12 +81,16 @@
       fixed="right"
       label="Option"
       width="180">
-        <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">DELETE</el-button>
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row), deleteEmployee(index)" type="text" size="small">DELETE</el-button>
         <el-button type="text" size="small">MODIFY</el-button>
+        
       </template>
     </el-table-column>
   </el-table>
+  </el-main>
+</container>
+  
 </template>
 
 <script>
@@ -55,15 +99,19 @@
       handleClick(row) {
         console.log(row);
       },
-      renderHeader () {
-        return (
-          <div>
-          <el-button size='small' on-click={()=>this.exportExcel()}> <span  class='el-icon-upload2'></span> ADD</el-button>
-          </div>
-          )
+      deleteEmployee(index){
+        this.tableData.splice(index,1);
+      } , 
+      onSubmit(){
+        this.tableData.push(this.form);
+        this.form='';
+      },
+      onCancel(){
+        resetField(form);
       }
     },
     data() {
+      
       const item = {
         id: '1',
         firstName: 'Chonghe',
@@ -73,7 +121,19 @@
         address: '4800 Calhoun Rd, Houston, TX 77004',
       };
       return {
-        tableData: Array(20).fill(item),
+        tableData: Array(10).fill(item),
+        dialogVisible:false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+
       };
     },
 
